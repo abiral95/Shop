ab29e655c0d1acc827bd4e38c8afce7e5769bf76
Abiral Poudel <abiral.p95@gmail.com>
2018-01-26 15:51:17 +0545

    halka

---
+++ Shop/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello from Index";
+        }
+        public string Browse(string category)
+        {
+            string message = HttpUtility.HtmlEncode("showing category " + category);
+            return message;
+        }
+        public ActionResult Details(int id)
+        {
+            var Item = new Item { Title = "Item" + id };
+            return View(Item);
+        }
+    }
+}
---
+++ Shop/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public Category Category { get; set; }
+    }
+}
